e9b25e1c5c177e83517ed4c3152e756740ef164d
Evan Sosenko <razorx@evansosenko.com>
2022-06-24 11:03:48 -0700

    Import all components

--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"github.com/benthosdev/benthos/v4/public/service"
+	_ "github.com/benthosdev/benthos/v4/public/components/all"
 	_ "github.com/makenew/benthos-plugin/v2/plugin"
 )
 
