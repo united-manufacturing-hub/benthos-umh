4bfcaebd88f399458d8d03cf4adfdc8ef9fbae88
JeremyTheocharis <jt@umh.app>
2023-03-14 20:57:56 +0100

    feat: working

--- plugin/opcua.go
+++ plugin/opcua.go
@@ -2,10 +2,164 @@ package plugin
 
 import (
 	"context"
-	"math/rand"
+	"encoding/json"
+	"strconv"
 
 	"github.com/benthosdev/benthos/v4/public/service"
+
+	"github.com/gopcua/opcua"
+	"github.com/gopcua/opcua/errors"
+	"github.com/gopcua/opcua/id"
+	"github.com/gopcua/opcua/ua"
 )
+
+type NodeDef struct {
+	NodeID      *ua.NodeID
+	NodeClass   ua.NodeClass
+	BrowseName  string
+	Description string
+	AccessLevel ua.AccessLevelType
+	Path        string
+	DataType    string
+	Writable    bool
+	Unit        string
+	Scale       string
+	Min         string
+	Max         string
+}
+
+func (n NodeDef) Records() []string {
+	return []string{n.BrowseName, n.DataType, n.NodeID.String(), n.Unit, n.Scale, n.Min, n.Max, strconv.FormatBool(n.Writable), n.Description}
+}
+
+func join(a, b string) string {
+	if a == "" {
+		return b
+	}
+	return a + "." + b
+}
+
+func browse(ctx context.Context, n *opcua.Node, path string, level int) ([]NodeDef, error) {
+	// fmt.Printf("node:%s path:%q level:%d\n", n, path, level)
+	if level > 10 {
+		return nil, nil
+	}
+
+	attrs, err := n.AttributesWithContext(ctx, ua.AttributeIDNodeClass, ua.AttributeIDBrowseName, ua.AttributeIDDescription, ua.AttributeIDAccessLevel, ua.AttributeIDDataType)
+	if err != nil {
+		return nil, err
+	}
+
+	var def = NodeDef{
+		NodeID: n.ID,
+	}
+
+	switch err := attrs[0].Status; err {
+	case ua.StatusOK:
+		def.NodeClass = ua.NodeClass(attrs[0].Value.Int())
+	default:
+		return nil, err
+	}
+
+	switch err := attrs[1].Status; err {
+	case ua.StatusOK:
+		def.BrowseName = attrs[1].Value.String()
+	default:
+		return nil, err
+	}
+
+	switch err := attrs[2].Status; err {
+	case ua.StatusOK:
+		def.Description = attrs[2].Value.String()
+	case ua.StatusBadAttributeIDInvalid:
+		// ignore
+	default:
+		return nil, err
+	}
+
+	switch err := attrs[3].Status; err {
+	case ua.StatusOK:
+		def.AccessLevel = ua.AccessLevelType(attrs[3].Value.Int())
+		def.Writable = def.AccessLevel&ua.AccessLevelTypeCurrentWrite == ua.AccessLevelTypeCurrentWrite
+	case ua.StatusBadAttributeIDInvalid:
+		// ignore
+	default:
+		return nil, err
+	}
+
+	switch err := attrs[4].Status; err {
+	case ua.StatusOK:
+		switch v := attrs[4].Value.NodeID().IntID(); v {
+		case id.DateTime:
+			def.DataType = "time.Time"
+		case id.Boolean:
+			def.DataType = "bool"
+		case id.SByte:
+			def.DataType = "int8"
+		case id.Int16:
+			def.DataType = "int16"
+		case id.Int32:
+			def.DataType = "int32"
+		case id.Byte:
+			def.DataType = "byte"
+		case id.UInt16:
+			def.DataType = "uint16"
+		case id.UInt32:
+			def.DataType = "uint32"
+		case id.UtcTime:
+			def.DataType = "time.Time"
+		case id.String:
+			def.DataType = "string"
+		case id.Float:
+			def.DataType = "float32"
+		case id.Double:
+			def.DataType = "float64"
+		default:
+			def.DataType = attrs[4].Value.NodeID().String()
+		}
+	case ua.StatusBadAttributeIDInvalid:
+		// ignore
+	default:
+		return nil, err
+	}
+
+	def.Path = join(path, def.BrowseName)
+	// fmt.Printf("%d: def.Path:%s def.NodeClass:%s\n", level, def.Path, def.NodeClass)
+
+	var nodes []NodeDef
+	if def.NodeClass == ua.NodeClassVariable {
+		nodes = append(nodes, def)
+	}
+
+	browseChildren := func(refType uint32) error {
+		refs, err := n.ReferencedNodesWithContext(ctx, refType, ua.BrowseDirectionForward, ua.NodeClassAll, true)
+		if err != nil {
+			return errors.Errorf("References: %d: %s", refType, err)
+		}
+		// fmt.Printf("found %d child refs\n", len(refs))
+		for _, rn := range refs {
+			children, err := browse(ctx, rn, def.Path, level+1)
+			if err != nil {
+				return errors.Errorf("browse children: %s", err)
+			}
+			nodes = append(nodes, children...)
+		}
+		return nil
+	}
+
+	if err := browseChildren(id.HasComponent); err != nil {
+		return nil, err
+	}
+	if err := browseChildren(id.Organizes); err != nil {
+		return nil, err
+	}
+	if err := browseChildren(id.HasProperty); err != nil {
+		return nil, err
+	}
+	return nodes, nil
+}
+
+//------------------------------------------------------------------------------
 
 var OPCUAConfigSpec = service.NewConfigSpec().
 	Summary("Creates an input that reads data from OPC-UA servers.").
@@ -22,7 +176,18 @@ func newOPCUAInput(conf *service.ParsedConfig) (service.Input, error) {
 	if err != nil {
 		return nil, err
 	}
-	return service.AutoRetryNacks(&OPCUAInput{endpoint, nodeID}), nil
+
+	id, err := ua.ParseNodeID(nodeID)
+	if err != nil {
+		return nil, err
+	}
+
+	m := &OPCUAInput{
+		endpoint: endpoint,
+		nodeID:   id,
+	}
+
+	return service.AutoRetryNacks(m), nil
 }
 
 func init() {
@@ -40,18 +205,39 @@ func init() {
 
 type OPCUAInput struct {
 	endpoint string
-	nodeID   string
+	nodeID   *ua.NodeID
+
+	client *opcua.Client
 }
 
 func (g *OPCUAInput) Connect(ctx context.Context) error {
+
+	if g.client != nil {
+		return nil
+	}
+
+	c := opcua.NewClient(g.endpoint)
+	if err := c.Connect(ctx); err != nil {
+		panic(err)
+	}
+
+	g.client = c
+
 	return nil
 }
 
 func (g *OPCUAInput) Read(ctx context.Context) (*service.Message, service.AckFunc, error) {
-	b := make([]byte, 100)
-	for k := range b {
-		b[k] = byte((rand.Int() % 94) + 32)
+
+	nodeList, err := browse(ctx, g.client.Node(g.nodeID), "", 0)
+	if err != nil {
+		panic(err)
 	}
+
+	b, err := json.Marshal(nodeList)
+	if err != nil {
+		panic(err)
+	}
+
 	return service.NewMessage(b), func(ctx context.Context, err error) error {
 		// Nacks are retried automatically when we use service.AutoRetryNacks
 		return nil
