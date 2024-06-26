e19c16ac9e13c4b33b78bdc4893d9c620f20850d
Ferdinand Linnenberg <ferdinand@linnenberg.dev>
2024-06-26 12:49:48 +0000

    feat: use bundle import

---
+++ bundle/package.go
@@ -0,0 +1,12 @@
+package bundle
+
+import (
+	// Fix for ENG-752
+	_ "github.com/redpanda-data/connect/public/bundle/free/v4"
+
+	_ "github.com/RuneRoven/benthosADS"
+	_ "github.com/RuneRoven/benthosSMTP"
+	_ "github.com/redpanda-data/benthos/v4/public/service"
+	_ "github.com/united-manufacturing-hub/benthos-umh/v2/opcua_plugin"
+	_ "github.com/united-manufacturing-hub/benthos-umh/v2/s7comm_plugin"
+)
--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -17,14 +17,7 @@ package main
 import (
 	"context"
 
-	// Fix for ENG-752
-	_ "github.com/redpanda-data/connect/public/bundle/free/v4"
-
-	_ "github.com/RuneRoven/benthosADS"
-	_ "github.com/RuneRoven/benthosSMTP"
-	"github.com/redpanda-data/benthos/v4/public/service"
-	_ "github.com/united-manufacturing-hub/benthos-umh/v2/opcua_plugin"
-	_ "github.com/united-manufacturing-hub/benthos-umh/v2/s7comm_plugin"
+	_ "github.com/united-manufacturing-hub/benthos-umh/v2/bundle"
 )
 
 func main() {
