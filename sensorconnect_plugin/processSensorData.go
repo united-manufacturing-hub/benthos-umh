ad386d8338f123d2e20d099ef2e19b3dfad3ee24
Jeremy Theocharis <jt@umh.app>
2024-10-21 22:44:09 +0000

    savegame

--- sensorconnect_plugin/processSensorData.go
+++ sensorconnect_plugin/processSensorData.go
@@ -2,11 +2,13 @@ package sensorconnect_plugin
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/redpanda-data/benthos/v4/public/service"
 	"go.uber.org/zap"
 	"math"
 	"math/big"
+	"reflect"
 	"strconv"
 )
 
@@ -73,35 +75,62 @@ func (s *SensorConnectInput) ProcessSensorData(ctx context.Context, portModeMap 
 			ioddFilemapKey.VendorId = int64(portMode.VendorID)
 
 			// check if entry for IoddFilemapKey exists in ioddIoDeviceMap
-			if _, ok := s.IoDeviceMap.Load(ioddFilemapKey); !ok {
+			ioddFile, ok := s.IoDeviceMap.Load(ioddFilemapKey)
+			if !ok {
 				zap.S().Debugf("IoddFilemapKey %v not in IodddeviceMap", ioddFilemapKey)
 				continue
 			}
 
+			cidm, ok := ioddFile.(IoDevice)
+			if !ok {
+				zap.S().Errorf("Failed to cast idm to IoDevice")
+				continue
+			}
+
 			// create padded binary raw sensor output
-			/*
-				rawSensorOutputLength := len(rawSensorOutput)
 
-					outputBitLength := rawSensorOutputLength * 4
-					rawSensorOutputString := string(rawSensorOutput)
-					rawSensorOutputBinary := HexToBin(rawSensorOutputString)
-					rawSensorOutputBinaryPadded := zeroPadding(rawSensorOutputBinary, outputBitLength)
+			rawSensorOutputLength := len(rawSensorOutput)
 
-					cidm, ok := idm.(IoDevice)
-					if !ok {
-						zap.S().Errorf("Failed to cast idm to IoDevice")
-						continue
-					}
+			outputBitLength := rawSensorOutputLength * 4
+			rawSensorOutputString := string(rawSensorOutput)
+			rawSensorOutputBinary, err := s.HexToBin(rawSensorOutputString)
+			if err != nil {
+				return nil, err
+			}
+			rawSensorOutputBinaryPadded := s.ZeroPadding(rawSensorOutputBinary, outputBitLength)
 
-					// Extract important IoddStruct parts for better readability
-					processDataIn := cidm.ProfileBody.DeviceFunction.ProcessDataCollection.ProcessData.ProcessDataIn
-					datatypeReferenceArray := cidm.ProfileBody.DeviceFunction.DatatypeCollection.DatatypeArray
-					var emptySimpleDatatype SimpleDatatype
-					primLangExternalTextCollection := cidm.ExternalTextCollection.PrimaryLanguage.Text
+			// Extract important IoddStruct parts for better readability
+			processDataIn := cidm.ProfileBody.DeviceFunction.ProcessDataCollection.ProcessData.ProcessDataIn
+			datatypeReferenceArray := cidm.ProfileBody.DeviceFunction.DatatypeCollection.DatatypeArray
+			var emptySimpleDatatype SimpleDatatype
+			primLangExternalTextCollection := cidm.ExternalTextCollection.PrimaryLanguage.Text
 
-			*/
+			// Process the data
+			payload, err := s.processData(
+				processDataIn.Datatype,
+				processDataIn.DatatypeRef,
+				emptySimpleDatatype,
+				0,
+				outputBitLength,
+				rawSensorOutputBinaryPadded,
+				datatypeReferenceArray,
+				processDataIn.Name.TextId,
+				primLangExternalTextCollection)
+			if err != nil {
+				s.logger.Errorf("Failed to process data: %v", err)
+				// Handle the error as needed
+			}
+
 			// Create message
-			message := service.NewMessage(rawSensorOutput)
+			b := make([]byte, 0)
+			jsonBytes, err := json.Marshal(payload)
+			if err != nil {
+				s.logger.Errorf("Error marshaling to JSON: %v", err)
+				return nil, err
+			}
+			b = append(b, jsonBytes...)
+
+			message := service.NewMessage(b)
 
 			message.MetaSet("sensorconnect_port_mode", "io-link")
 			message.MetaSet("sensorconnect_port_number", portNumberString)
@@ -152,9 +181,9 @@ func (s *SensorConnectInput) extractIntFromSensorDataMap(key string, tag string,
 	return int(val), nil
 }
 
-// convertBinaryValue converts a binary string to its corresponding value based on the datatype.
+// ConvertBinaryValue converts a binary string to its corresponding value based on the datatype.
 // It handles both string and numeric data types and logs errors using the Benthos logger.
-func (s *SensorConnectInput) convertBinaryValue(binaryValue string, datatype string) (interface{}, error) {
+func (s *SensorConnectInput) ConvertBinaryValue(binaryValue string, datatype string) (interface{}, error) {
 	bitLen := len(binaryValue)
 	raw, err := strconv.ParseUint(binaryValue, 2, bitLen)
 	if err != nil {
@@ -166,7 +195,10 @@ func (s *SensorConnectInput) convertBinaryValue(binaryValue string, datatype str
 
 	switch datatype {
 	case "OctetStringT":
-		output = BinToHex(binaryValue)
+		output, err = s.BinToHex(binaryValue)
+		if err != nil {
+			return nil, err
+		}
 	case "UIntegerT":
 		output = raw
 	case "IntegerT":
@@ -189,32 +221,77 @@ func (s *SensorConnectInput) convertBinaryValue(binaryValue string, datatype str
 		output = raw == 1
 	default:
 		s.logger.Warnf("Datatype %s not supported", datatype)
-		output = BinToHex(binaryValue)
+		output, err = s.BinToHex(binaryValue)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return output, nil
 }
 
-// BinToHex converts a binary string to a hex string
-func BinToHex(bin string) (hex string) {
+// BinToHex converts a binary string to its hexadecimal representation.
+// It preserves leading zeros based on the length of the input binary string.
+// Returns an error if the binary string is invalid.
+func (s *SensorConnectInput) BinToHex(bin string) (string, error) {
+	// Validate that the binary string contains only '0' and '1'
+	for _, char := range bin {
+		if char != '0' && char != '1' {
+			s.logger.Errorf("Invalid character '%c' in binary string: %s", char, bin)
+			return "", fmt.Errorf("invalid character '%c' in binary string", char)
+		}
+	}
+
+	// Convert binary string to big.Int
 	i := new(big.Int)
-	i.SetString(bin, 2)
-	hex = fmt.Sprintf("%x", i)
-	return
+	_, ok := i.SetString(bin, 2)
+	if !ok {
+		s.logger.Errorf("Failed to parse binary string: %s", bin)
+		return "", fmt.Errorf("invalid binary string: %s", bin)
+	}
+
+	// Calculate the number of hexadecimal digits required
+	// Each hex digit represents 4 bits
+	hexDigits := (len(bin) + 3) / 4 // Ceiling division to account for any remaining bits
+
+	// Convert to hexadecimal string with leading zeros
+	hex := fmt.Sprintf("%0*x", hexDigits, i)
+
+	return hex, nil
 }
 
-// zeroPadding adds zeros on the left side of a string until the lengt of the string equals the requested length
-func zeroPadding(input string, length int) (output string) {
-	output = fmt.Sprintf("%0*v", length, input)
-	return
+// HexToBin converts a hexadecimal string to its binary representation.
+// It preserves leading zeros based on the length of the input hexadecimal string.
+// Returns an error if the hexadecimal string is invalid.
+func (s *SensorConnectInput) HexToBin(hex string) (string, error) {
+	// Convert hex string to big.Int
+	i := new(big.Int)
+	_, ok := i.SetString(hex, 16)
+	if !ok {
+		s.logger.Errorf("Failed to parse hexadecimal string: %s", hex)
+		return "", fmt.Errorf("invalid hexadecimal string: %s", hex)
+	}
+
+	// Calculate the number of bits required
+	// Each hex digit represents 4 bits
+	bitLength := len(hex) * 4
+
+	// Convert to binary string with leading zeros
+	bin := fmt.Sprintf("%0*b", bitLength, i)
+
+	return bin, nil
 }
 
-// HexToBin converts a hex string into a binary string
-func HexToBin(hex string) (bin string) {
-	i := new(big.Int)
-	i.SetString(hex, 16)
-	bin = fmt.Sprintf("%b", i)
-	return
+// ZeroPadding pads the input string with leading zeros until it reaches the desired length.
+func (s *SensorConnectInput) ZeroPadding(input string, desiredLength int) string {
+	if len(input) >= desiredLength {
+		return input
+	}
+	padding := make([]byte, desiredLength-len(input))
+	for i := range padding {
+		padding[i] = '0'
+	}
+	return string(padding) + input
 }
 
 // determineValueBitLength returns the bitlength of a value
@@ -226,4 +303,238 @@ func determineValueBitLength(datatype string, bitLength uint, fixedLength uint) 
 	} else {
 		return bitLength
 	}
+}
+
+// processData turns raw sensor data into human-readable data.
+// It handles the input of Datatype, DatatypeRef, and SimpleDatatype structures.
+// It determines which one is provided and delegates processing accordingly.
+func (s *SensorConnectInput) processData(
+	datatype Datatype,
+	datatypeRef DatatypeRef,
+	simpleDatatype SimpleDatatype,
+	bitOffset int,
+	outputBitLength int,
+	rawSensorOutputBinaryPadded string,
+	datatypeReferenceArray []Datatype,
+	nameTextId string,
+	primLangExternalTextCollection []Text) (map[string]interface{}, error) {
+
+	var payload map[string]interface{}
+	var err error
+
+	if !s.isEmpty(simpleDatatype) {
+		payload, err = s.processSimpleDatatype(
+			simpleDatatype,
+			outputBitLength,
+			rawSensorOutputBinaryPadded,
+			bitOffset,
+			nameTextId,
+			primLangExternalTextCollection)
+		if err != nil {
+			s.logger.Errorf("Error with processSimpleDatatype: %v", err)
+			return nil, err
+		}
+		return payload, nil
+	} else if !s.isEmpty(datatype) {
+		payload, err = s.processDatatype(
+			datatype,
+			outputBitLength,
+			rawSensorOutputBinaryPadded,
+			bitOffset,
+			datatypeReferenceArray,
+			nameTextId,
+			primLangExternalTextCollection)
+		if err != nil {
+			s.logger.Errorf("Error with processDatatype: %v", err)
+			return nil, err
+		}
+		return payload, nil
+	} else if !s.isEmpty(datatypeRef) {
+		datatype, err = s.getDatatypeFromDatatypeRef(datatypeRef, datatypeReferenceArray)
+		if err != nil {
+			s.logger.Errorf("Error with getDatatypeFromDatatypeRef: %v", err)
+			return nil, err
+		}
+		payload, err = s.processDatatype(
+			datatype,
+			outputBitLength,
+			rawSensorOutputBinaryPadded,
+			bitOffset,
+			datatypeReferenceArray,
+			nameTextId,
+			primLangExternalTextCollection)
+		if err != nil {
+			s.logger.Errorf("Error with processDatatype: %v", err)
+			return nil, err
+		}
+		return payload, nil
+	} else {
+		s.logger.Errorf("Missing input, neither SimpleDatatype, Datatype, nor DatatypeRef provided.")
+		return nil, fmt.Errorf("missing input, neither SimpleDatatype, Datatype, nor DatatypeRef provided")
+	}
+}
+
+// getDatatypeFromDatatypeRef finds the actual Datatype description in the datatypeReferenceArray using the given DatatypeRef.
+func (s *SensorConnectInput) getDatatypeFromDatatypeRef(datatypeRef DatatypeRef, datatypeReferenceArray []Datatype) (Datatype, error) {
+	for _, datatypeElement := range datatypeReferenceArray {
+		if datatypeElement.Id == datatypeRef.DatatypeId {
+			return datatypeElement, nil
+		}
+	}
+	s.logger.Errorf("DatatypeRef.DatatypeId %s is not in DatatypeCollection of IODD file.", datatypeRef.DatatypeId)
+	return Datatype{}, fmt.Errorf("did not find Datatype structure for given datatype reference id: %v", datatypeRef.DatatypeId)
+}
+
+// processSimpleDatatype processes the given SimpleDatatype and returns the payload.
+func (s *SensorConnectInput) processSimpleDatatype(
+	simpleDatatype SimpleDatatype,
+	outputBitLength int,
+	rawSensorOutputBinaryPadded string,
+	bitOffset int,
+	nameTextId string,
+	primLangExternalTextCollection []Text) (map[string]interface{}, error) {
+
+	payload := make(map[string]interface{})
+
+	binaryValue := s.extractBinaryValueFromRawSensorOutput(
+		rawSensorOutputBinaryPadded,
+		simpleDatatype.Type,
+		simpleDatatype.BitLength,
+		simpleDatatype.FixedLength,
+		outputBitLength,
+		bitOffset)
+	valueString, err := s.ConvertBinaryValue(binaryValue, simpleDatatype.Type)
+	if err != nil {
+		return nil, err
+	}
+	valueName := s.getNameFromExternalTextCollection(nameTextId, primLangExternalTextCollection)
+	payload[valueName] = valueString
+	return payload, nil
+}
+
+// extractBinaryValueFromRawSensorOutput handles the extraction of the actual raw sensor data.
+func (s *SensorConnectInput) extractBinaryValueFromRawSensorOutput(
+	rawSensorOutputBinaryPadded string,
+	typeString string,
+	bitLength uint,
+	fixedLength uint,
+	outputBitLength int,
+	bitOffset int) string {
+
+	valueBitLength := s.determineValueBitLength(typeString, bitLength, fixedLength)
+
+	leftIndex := outputBitLength - int(valueBitLength) - bitOffset
+	rightIndex := outputBitLength - bitOffset
+	binaryValue := rawSensorOutputBinaryPadded[leftIndex:rightIndex]
+	return binaryValue
+}
+
+// processDatatype processes a Datatype structure and returns the payload.
+func (s *SensorConnectInput) processDatatype(
+	datatype Datatype,
+	outputBitLength int,
+	rawSensorOutputBinaryPadded string,
+	bitOffset int,
+	datatypeReferenceArray []Datatype,
+	nameTextId string,
+	primLangExternalTextCollection []Text) (map[string]interface{}, error) {
+
+	if datatype.Type == "RecordT" {
+		payload, err := s.processRecordType(
+			datatype.RecordItemArray,
+			outputBitLength,
+			rawSensorOutputBinaryPadded,
+			datatypeReferenceArray,
+			primLangExternalTextCollection)
+		if err != nil {
+			s.logger.Errorf("Error with processRecordType: %v", err)
+			return nil, err
+		}
+		return payload, nil
+	} else {
+		binaryValue := s.extractBinaryValueFromRawSensorOutput(
+			rawSensorOutputBinaryPadded,
+			datatype.Type,
+			datatype.BitLength,
+			datatype.FixedLength,
+			outputBitLength,
+			bitOffset)
+		valueString, err := s.ConvertBinaryValue(binaryValue, datatype.Type)
+		if err != nil {
+			return nil, err
+		}
+		valueName := s.getNameFromExternalTextCollection(nameTextId, primLangExternalTextCollection)
+		payload := make(map[string]interface{})
+		payload[valueName] = valueString
+		return payload, nil
+	}
+}
+
+// processRecordType iterates through the given RecordItemArray and processes each RecordItem.
+func (s *SensorConnectInput) processRecordType(
+	recordItemArray []RecordItem,
+	outputBitLength int,
+	rawSensorOutputBinaryPadded string,
+	datatypeReferenceArray []Datatype,
+	primLangExternalTextCollection []Text) (map[string]interface{}, error) {
+
+	payload := make(map[string]interface{})
+
+	for _, element := range recordItemArray {
+		var datatypeEmpty Datatype
+		itemPayload, err := s.processData(
+			datatypeEmpty,
+			element.DatatypeRef,
+			element.SimpleDatatype,
+			element.BitOffset,
+			outputBitLength,
+			rawSensorOutputBinaryPadded,
+			datatypeReferenceArray,
+			element.Name.TextId,
+			primLangExternalTextCollection)
+		if err != nil {
+			s.logger.Errorf("Processing of RecordItem failed: %v", err)
+			continue
+		}
+		// Merge itemPayload into payload
+		for k, v := range itemPayload {
+			payload[k] = v
+		}
+	}
+	return payload, nil
+}
+
+// isEmpty determines if a field of a struct is empty.
+func (s *SensorConnectInput) isEmpty(object interface{}) bool {
+	if object == nil || object == "" || object == false {
+		return true
+	}
+
+	val := reflect.ValueOf(object)
+	if val.Kind() == reflect.Struct {
+		empty := reflect.New(val.Type()).Elem().Interface()
+		return reflect.DeepEqual(object, empty)
+	}
+	return false
+}
+
+// determineValueBitLength returns the bit length of a value based on its datatype.
+func (s *SensorConnectInput) determineValueBitLength(datatype string, bitLength uint, fixedLength uint) uint {
+	if datatype == "BooleanT" {
+		return 1
+	} else if datatype == "OctetStringT" {
+		return fixedLength * 8
+	} else {
+		return bitLength
+	}
+}
+
+// getNameFromExternalTextCollection retrieves the name corresponding to a textId from the IODD TextCollection.
+func (s *SensorConnectInput) getNameFromExternalTextCollection(textId string, texts []Text) string {
+	for _, element := range texts {
+		if textId == element.Id {
+			return element.Value
+		}
+	}
+	return "error: translation not found"
 }
---
+++ sensorconnect_plugin/processSensorData_test.go
@@ -0,0 +1,166 @@
+package sensorconnect_plugin_test
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"math"
+
+	"github.com/united-manufacturing-hub/benthos-umh/v2/sensorconnect_plugin"
+)
+
+var _ = Describe("ProcessSensorData", func() {
+
+	var (
+		s *sensorconnect_plugin.SensorConnectInput
+	)
+
+	BeforeEach(func() {
+		// Initialize SensorConnectInput with a no-op logger or a mock logger
+		// Here, we'll assume that a logger can be initialized as follows:
+		// Adjust this according to your actual logger initialization
+		s = &sensorconnect_plugin.SensorConnectInput{}
+	})
+
+	Describe("Bit Conversions", func() {
+		It("should correctly convert hex to binary and back with padding", func() {
+			longHexString := "0000FC000001FF000000FF000161FF000025FF03"
+			length := len(longHexString) * 4
+			bitString, err := s.HexToBin(longHexString)
+			Expect(err).ToNot(HaveOccurred())
+			fmt.Println(bitString)
+			bitStringPadded := s.ZeroPadding(bitString, length)
+			fmt.Println(bitStringPadded)
+			endHexString, err := s.BinToHex(bitStringPadded)
+			Expect(err).ToNot(HaveOccurred())
+			endHexStringPadded := s.ZeroPadding(endHexString, len(longHexString))
+			fmt.Println(endHexStringPadded)
+			fmt.Println(longHexString)
+			Expect(endHexStringPadded).To(Equal("0000fc000001ff000000ff000161ff000025ff03"), "Problem with BitConversions")
+		})
+	})
+
+	Describe("BooleanT Conversions", func() {
+		tests := []struct {
+			binary   string
+			expected bool
+		}{
+			{
+				binary:   "1",
+				expected: true,
+			},
+			{
+				binary:   "0",
+				expected: false,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt // Capture range variable
+			It(fmt.Sprintf("should convert binary '%s' to bool '%t'", tt.binary, tt.expected), func() {
+				result, err := s.ConvertBinaryValue(tt.binary, "BooleanT")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(Equal(tt.expected))
+			})
+		}
+	})
+
+	Describe("Float32T Conversions", func() {
+		tests := []struct {
+			binary   string
+			expected float32
+		}{
+			{
+				binary:   "01000000010000000000000000000000",
+				expected: 3.0,
+			},
+			{
+				binary:   "11000000010000000000000000000000",
+				expected: -3.0,
+			},
+			{
+				binary:   "00000000000000000000000000000000",
+				expected: 0.0,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt // Capture range variable
+			It(fmt.Sprintf("should convert binary '%s' to float32 '%f'", tt.binary, tt.expected), func() {
+				result, err := s.ConvertBinaryValue(tt.binary, "Float32T")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(Equal(tt.expected))
+			})
+		}
+	})
+
+	Describe("UIntegerT Conversions", func() {
+		tests := []struct {
+			binary   string
+			expected uint64
+		}{
+			{
+				binary:   "0001101",
+				expected: 13,
+			},
+			{
+				binary:   "10110111001011010",
+				expected: 93786,
+			},
+			{
+				binary:   "11111111111111111111111111111111",
+				expected: math.MaxUint32,
+			},
+			{
+				binary:   "1111111111111111111111111111111111111111111111111111111111111111",
+				expected: math.MaxUint64,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt // Capture range variable
+			It(fmt.Sprintf("should convert binary '%s' to uint64 '%v'", tt.binary, tt.expected), func() {
+				result, err := s.ConvertBinaryValue(tt.binary, "UIntegerT")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(Equal(tt.expected))
+			})
+		}
+	})
+
+	Describe("IntegerT Conversions", func() {
+		tests := []struct {
+			binary   string
+			expected int
+		}{
+			{
+				binary:   "10000000",
+				expected: math.MinInt8,
+			},
+			{
+				binary:   "01111111",
+				expected: math.MaxInt8,
+			},
+			{
+				binary:   "1000000000000000",
+				expected: math.MinInt16,
+			},
+			{
+				binary:   "10000000000000000000000000000000",
+				expected: math.MinInt32,
+			},
+			{
+				binary:   "1000000000000000000000000000000000000000000000000000000000000000",
+				expected: math.MinInt64,
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt // Capture range variable
+			It(fmt.Sprintf("should convert binary '%s' to int '%v'", tt.binary, tt.expected), func() {
+				result, err := s.ConvertBinaryValue(tt.binary, "IntegerT")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(result).To(Equal(tt.expected))
+			})
+		}
+	})
+})
