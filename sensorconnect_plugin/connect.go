fe0d2a7db6fac820e680e52799e5e03a689583f6
Jeremy Theocharis <jt@umh.app>
2024-10-23 16:42:14 +0000

    fix: detection for bugged firmware

--- sensorconnect_plugin/connect.go
+++ sensorconnect_plugin/connect.go
@@ -3,6 +3,9 @@ package sensorconnect_plugin
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 // DiscoverResponseFromDevice structures for parsing device responses
@@ -23,9 +26,11 @@ type StringDataPoint struct {
 
 // DeviceInformation holds relevant information about discovered devices
 type DeviceInformation struct {
-	ProductCode  string
-	SerialNumber string
-	URL          string
+	ProductCode     string
+	SerialNumber    string
+	URL             string
+	FirmwareVersion string
+	BuggedFirmware  bool // This indicates if the firmware is known to have crash issues if one does too many requests. Set to true if the firmware version is not known.
 }
 
 // GetDeviceInformation connects to the device and retrieves its information
@@ -35,7 +40,7 @@ func (s *SensorConnectInput) GetDeviceInformation(ctx context.Context) (DeviceIn
 		"adr":  "/getdatamulti",
 		"data": map[string]interface{}{
 			"datatosend": []string{
-				"/deviceinfo/serialnumber/", "/deviceinfo/productcode/",
+				"/deviceinfo/serialnumber/", "/deviceinfo/productcode/", "/deviceinfo/swrevision/",
 			},
 		},
 	}
@@ -87,6 +92,12 @@ func (s *SensorConnectInput) GetDeviceInformation(ctx context.Context) (DeviceIn
 		return DeviceInformation{}, err
 	}
 
+	firmwareVersionCodeDP, err := getStringDataPoint("/deviceinfo/swrevision/")
+	if err != nil {
+		s.logger.Errorf("Error extracting firmware: %v", err)
+		return DeviceInformation{}, err
+	}
+
 	if productCodeDP.Code != 200 {
 		s.logger.Errorf("Error extracting product code, responded with code %v: %v", GetDiagnosticMessage(productCodeDP.Code), productCodeDP.Data)
 		return DeviceInformation{}, fmt.Errorf("error extracting product code, responded with code %v: %v", GetDiagnosticMessage(productCodeDP.Code), productCodeDP.Data)
@@ -97,12 +108,86 @@ func (s *SensorConnectInput) GetDeviceInformation(ctx context.Context) (DeviceIn
 		return DeviceInformation{}, fmt.Errorf("error extracting serial number, responded with code %v: %v", GetDiagnosticMessage(serialNumberDP.Code), serialNumberDP.Data)
 	}
 
-	deviceInfo := DeviceInformation{
-		ProductCode:  productCodeDP.Data,
-		SerialNumber: serialNumberDP.Data,
-		URL:          fmt.Sprintf("http://%s", s.DeviceAddress),
+	if firmwareVersionCodeDP.Code != 200 {
+		s.logger.Errorf("Error extracting firmware version, responded with code %v: %v", GetDiagnosticMessage(firmwareVersionCodeDP.Code), firmwareVersionCodeDP.Data)
+		return DeviceInformation{}, fmt.Errorf("error extracting firmware version, responded with code %v: %v", GetDiagnosticMessage(firmwareVersionCodeDP.Code), firmwareVersionCodeDP.Data)
 	}
-	s.logger.Infof("Found device at %s (SN: %s, PN: %s)", deviceInfo.URL, deviceInfo.SerialNumber, deviceInfo.ProductCode)
+
+	buggedFirmware := detectBuggedFirmware(firmwareVersionCodeDP.Data)
+
+	deviceInfo := DeviceInformation{
+		ProductCode:     productCodeDP.Data,
+		SerialNumber:    serialNumberDP.Data,
+		URL:             fmt.Sprintf("http://%s", s.DeviceAddress),
+		FirmwareVersion: firmwareVersionCodeDP.Data,
+		BuggedFirmware:  buggedFirmware,
+	}
+	s.logger.Infof(
+		"Device detected at %s [Serial Number: %s, Product Code: %s, Firmware Version: %s, Bugged Firmware: %v]",
+		deviceInfo.URL,
+		deviceInfo.SerialNumber,
+		deviceInfo.ProductCode,
+		deviceInfo.FirmwareVersion,
+		deviceInfo.BuggedFirmware,
+	)
+
+	if buggedFirmware {
+		s.logger.Infof(
+			"You are using a firmware version for your IFM IO-Link master that may crash when handling excessive requests. " +
+				"IFM has not yet released an official fix; however, an experimental firmware provided by IFM and available through UMH addresses this issue. " +
+				"Please contact us for more information.",
+		)
+	}
 
 	return deviceInfo, nil
+}
+
+// detectBuggedFirmware determines if the firmware is bugged based on the version
+// The following firmware versions are not affected by the bug:
+// - AL1x5x_cn_it_v3.4.84
+// - AL1x4x_cn_it_v3.4.84
+// These versions were provided by IFM to UMH in 2021 but were not officially released at that time and have not been released (as of 2024-10-23)
+func detectBuggedFirmware(firmware string) bool {
+	referenceVersion := []int{3, 4, 84}
+
+	// Regular expression to extract version number after '_v'
+	re := regexp.MustCompile(`_v(\d+\.\d+\.\d+)$`)
+	matches := re.FindStringSubmatch(firmware)
+	if len(matches) != 2 {
+		// Unknown firmware format
+		return true
+	}
+
+	// Split the version into major, minor, patch
+	versionParts := strings.Split(matches[1], ".")
+	if len(versionParts) != 3 {
+		// Incorrect version format
+		return true
+	}
+
+	// Convert version parts to integers
+	versionNums := make([]int, 3)
+	for i, part := range versionParts {
+		num, err := strconv.Atoi(part)
+		if err != nil {
+			// Non-integer version part
+			return true
+		}
+		versionNums[i] = num
+	}
+
+	// Compare the extracted version with the reference version
+	for i := 0; i < 3; i++ {
+		if versionNums[i] > referenceVersion[i] {
+			// Version is greater than reference
+			return false
+		} else if versionNums[i] < referenceVersion[i] {
+			// Version is less than reference
+			return true
+		}
+		// If equal, continue to next part
+	}
+
+	// Version is exactly equal to reference
+	return false
 }
