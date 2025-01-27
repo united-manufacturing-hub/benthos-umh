31a236eda3f418c5af11fbba63fcad1ae2b53a6f
Janik Knodel <10290002+led0nk@users.noreply.github.com>
2025-01-27 15:14:05 +0100

    kepware_testing + workflow

--- opcua_plugin/opcua_kepware_test.go
+++ opcua_plugin/opcua_kepware_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"os"
-	"reflect"
 	"time"
 
 	"github.com/gopcua/opcua/ua"
@@ -22,20 +21,6 @@ var _ = Describe("Test against KepServer EX6", func() {
 	var input *OPCUAInput
 	var ctx context.Context
 	var cancel context.CancelFunc
-
-	type KepwareWrapper struct {
-		Username                     string
-		Password                     string
-		NodeIDs                      []*ua.NodeID
-		SecurityMode                 string
-		SecurityPolicy               string
-		BrowseHierarchicalReferences bool
-		AutoReconnect                bool
-		ReconnectIntervalInSeconds   int
-		SubscribeEnabled             bool
-		ErrorOccured                 bool
-		ExpectedValue                interface{}
-	}
 
 	BeforeEach(func() {
 		endpoint = os.Getenv("TEST_KEPWARE_ENDPOINT")
@@ -61,24 +46,13 @@ var _ = Describe("Test against KepServer EX6", func() {
 		}
 	})
 
-	//TODO: proper var handling for tableEntries since this wouldn't work
-	DescribeTable("Connect", func(wrapper KepwareWrapper) {
+	DescribeTable("Connect", func(opcInput *OPCUAInput, errorExpected bool) {
 
-		input = &OPCUAInput{
-			Endpoint:                     endpoint,
-			Username:                     wrapper.Username,
-			Password:                     wrapper.Password,
-			NodeIDs:                      wrapper.NodeIDs,
-			SecurityMode:                 wrapper.SecurityMode,
-			SecurityPolicy:               wrapper.SecurityPolicy,
-			BrowseHierarchicalReferences: wrapper.BrowseHierarchicalReferences,
-			AutoReconnect:                wrapper.AutoReconnect,
-			ReconnectIntervalInSeconds:   wrapper.ReconnectIntervalInSeconds,
-			SubscribeEnabled:             wrapper.SubscribeEnabled,
-		}
+		input = opcInput
+		input.Endpoint = endpoint
 
 		err := input.Connect(ctx)
-		if wrapper.ErrorOccured {
+		if errorExpected {
 			Expect(err).To(HaveOccurred())
 			return
 		}
@@ -138,136 +112,191 @@ var _ = Describe("Test against KepServer EX6", func() {
 		}
 
 	},
-		Entry("should connect", KepwareWrapper{
+		Entry("should connect", &OPCUAInput{
 			Username:                   "",
 			Password:                   "",
 			NodeIDs:                    nil,
 			SubscribeEnabled:           false,
 			AutoReconnect:              true,
 			ReconnectIntervalInSeconds: 5,
-			ErrorOccured:               false,
-		}),
-		Entry("should connect in no security mode", KepwareWrapper{
+		}, false),
+		Entry("should connect in no security mode", &OPCUAInput{
 			Username:         "",
 			Password:         "",
 			NodeIDs:          nil,
 			SubscribeEnabled: false,
 			SecurityMode:     "None",
 			SecurityPolicy:   "None",
-			ErrorOccured:     false,
-		}),
-		Entry("should connect with correct credentials", KepwareWrapper{
-			Username:     username,
-			Password:     password,
-			NodeIDs:      nil,
-			ErrorOccured: false,
-		}),
-		Entry("should fail to connect using incorrect credentials", KepwareWrapper{
-			Username:     "123",
-			Password:     "123",
-			NodeIDs:      nil,
-			ErrorOccured: true,
-		}),
-		Entry("should return a batch of messages", KepwareWrapper{
+		}, false),
+		Entry("should connect with correct credentials", &OPCUAInput{
+			Username: username,
+			Password: password,
+			NodeIDs:  nil,
+		}, false),
+		Entry("should fail to connect using incorrect credentials", &OPCUAInput{
+			Username: "123",
+			Password: "123",
+			NodeIDs:  nil,
+		}, true),
+		Entry("should return a batch of messages", &OPCUAInput{
 			Username:                     "",
 			Password:                     "",
 			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=Tests.TestDevice.testChangingData"}),
 			BrowseHierarchicalReferences: true,
 			AutoReconnect:                true,
 			ReconnectIntervalInSeconds:   5,
-			ErrorOccured:                 false,
-		}),
-		Entry("should return data changes on subscribe", KepwareWrapper{
+		}, false),
+		Entry("should return data changes on subscribe", &OPCUAInput{
 			Username:                     "",
 			Password:                     "",
 			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=Tests.TestDevice.testChangingData", "ns=2;s=Tests.TestDevice.testConstData"}),
 			BrowseHierarchicalReferences: true,
 			SubscribeEnabled:             true,
-			ErrorOccured:                 false,
-		}),
+		}, false),
+	)
+})
+
+var _ = Describe("Test underlying OPC-clients", func() {
+	var endpoint string
+	var username string
+	var password string
+	var input *OPCUAInput
+	var ctx context.Context
+	var cancel context.CancelFunc
+
+	BeforeEach(func() {
+		endpoint = os.Getenv("TEST_KEPWARE_ENDPOINT")
+		username = os.Getenv("TEST_KEPWARE_USERNAME")
+		password = os.Getenv("TEST_KEPWARE_PASSWORD")
+
+		if endpoint == "" || username == "" || password == "" {
+			Skip("Skipping test: environmental variables are not set")
+			return
+		}
+
+		ctx, cancel = context.WithTimeout(context.Background(), 60*time.Second)
+	})
+
+	AfterEach(func() {
+		if input.Client != nil {
+			err := input.Client.Close(ctx)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		if cancel != nil {
+			cancel()
+		}
+	})
+
+	DescribeTable("Test if Siemens-Namespace is available", func(namespace string, contains bool) {
+		input = &OPCUAInput{
+			Endpoint:                   endpoint,
+			Username:                   username,
+			Password:                   password,
+			AutoReconnect:              true,
+			ReconnectIntervalInSeconds: 5,
+		}
+
+		err := input.Connect(ctx)
+		Expect(err).NotTo(HaveOccurred())
+
+		req := &ua.ReadRequest{
+			NodesToRead: []*ua.ReadValueID{
+				{
+					NodeID:      ua.NewStringNodeID(2, "SiemensPLC_main.main.Server.NamespaceArray"),
+					AttributeID: ua.AttributeIDValue,
+				},
+			},
+		}
+
+		resp, err := input.Read(ctx, req)
+		Expect(err).NotTo(HaveOccurred())
+
+		namespaces, ok := resp.Results[0].Value.Value().([]string)
+		Expect(ok).To(Equal(true))
+
+		if !contains {
+			Expect(namespaces).NotTo(ContainElement(namespace))
+			return
+		}
+		Expect(namespaces).To(ContainElement(namespace))
+	},
+		Entry("should contain siemens-namespace", "http://Server _interface_1", true),
+		Entry("should fail due to incorrect namespace", "totally wrong namespace", false),
 	)
 
-	//TODO: iterate over the message to check for the correct namespace here
-	DescribeTable("Check PLC-data", func(wrapper KepwareWrapper) {
-		//NOTE: namespaceArray consists at least of http://Server_interface_1
-		// check on isDiederikCool == true
-		// check on counter getting data changes
+	DescribeTable("check for correct values", func(opcInput *OPCUAInput, expectedValue interface{}, dynamic bool) {
 
-		input = &OPCUAInput{
-			Endpoint:                     endpoint,
-			Username:                     wrapper.Username,
-			Password:                     wrapper.Password,
-			NodeIDs:                      wrapper.NodeIDs,
-			BrowseHierarchicalReferences: wrapper.BrowseHierarchicalReferences,
-			AutoReconnect:                wrapper.AutoReconnect,
-			ReconnectIntervalInSeconds:   wrapper.ReconnectIntervalInSeconds,
-		}
+		input = opcInput
+		input.Endpoint = endpoint
 
 		err := input.Connect(ctx)
 		Expect(err).NotTo(HaveOccurred())
 
 		var messageBatch service.MessageBatch
 
+		if !dynamic {
+			Eventually(func() (int, error) {
+				messageBatch, _, err = input.ReadBatch(ctx)
+				return len(messageBatch), err
+			}, 30*time.Second, 100*time.Millisecond).WithContext(ctx).Should(Equal(len(input.NodeIDs)))
+
+			for _, message := range messageBatch {
+				message, err := message.AsStructuredMut()
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(message).To(BeAssignableToTypeOf(expectedValue))
+				Expect(message).To(Equal(expectedValue))
+			}
+			return
+		}
 		Eventually(func() (int, error) {
 			messageBatch, _, err = input.ReadBatch(ctx)
 			return len(messageBatch), err
 		}, 30*time.Second, 100*time.Millisecond).WithContext(ctx).Should(Equal(len(input.NodeIDs)))
 
+		var storedMessage any
+
 		for _, message := range messageBatch {
 			message, err := message.AsStructuredMut()
-			test := reflect.TypeOf(message)
-
-			GinkgoWriter.Printf("type of message: %v\n", test)
-			GinkgoWriter.Printf("message from messageBatch:  %v\n", message)
-
 			Expect(err).NotTo(HaveOccurred())
+			var assignableNumber json.Number = "10.0"
 
-			//	if reflect.TypeOf(wrapper.ExpectedValue).Kind() == reflect.Slice {
-			//		for _, messageEntry := range message {
-			//			if messageEntry == wrapper.ExpectedValue {
-			//				err = fmt.Errorf("namespace not found")
-			//			}
-			//		}
+			Expect(message).To(BeAssignableToTypeOf(assignableNumber))
+			storedMessage = message
+		}
 
-			//	}
-			value := reflect.ValueOf(message)
-			GinkgoWriter.Printf("value of message: %v\n", value)
+		var messageBatch2 service.MessageBatch
+		Eventually(func() (int, error) {
+			messageBatch2, _, err = input.ReadBatch(ctx)
+			return len(messageBatch2), err
+		}, 30*time.Second, 100*time.Millisecond).WithContext(ctx).Should(Equal(len(input.NodeIDs)))
 
-			typeOf := reflect.TypeOf(wrapper.ExpectedValue).Kind()
-			GinkgoWriter.Printf("type of ExpectedValue: %v\n", typeOf)
-			Expect(message).To(BeAssignableToTypeOf(wrapper.ExpectedValue))
-			Expect(message).To(Equal(wrapper.ExpectedValue))
+		for _, message := range messageBatch2 {
+			message, err := message.AsStructuredMut()
+			Expect(err).NotTo(HaveOccurred())
+			var assignableNumber json.Number = "10.0"
+
+			Expect(message).To(BeAssignableToTypeOf(assignableNumber))
+			Expect(message).NotTo(Equal(storedMessage))
 		}
 
 	},
-		Entry("should return true for data-value", KepwareWrapper{
+		Entry("should return true", &OPCUAInput{
 			Username:                     "",
 			Password:                     "",
-			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=SiemensPLC_main.main.ServerInterfaces.Server _interface_1.custom_struct.Is_Diederik_Cool"}),
+			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=SiemensPLC_main.main.ServerInterfaces.Server _interface_1.test"}),
 			BrowseHierarchicalReferences: true,
 			AutoReconnect:                true,
 			ReconnectIntervalInSeconds:   5,
-			ErrorOccured:                 false,
-			ExpectedValue:                true,
-		}),
-		//		Entry("should receive data changes", KepwareWrapper{
-		//			Username:                     "",
-		//			Password:                     "",
-		//			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=SiemensPLC_main.main.ServerInterfaces.Server_interface_1.counter"}),
-		//			BrowseHierarchicalReferences: true,
-		//			AutoReconnect:                true,
-		//			ReconnectIntervalInSeconds:   5,
-		//			ErrorOccured:                 false,
-		//		}),
-		Entry("namespaceArray should consist of expected namespace", KepwareWrapper{
+		}, true, false),
+		Entry("should return data changes on subscribe", &OPCUAInput{
 			Username:                     "",
 			Password:                     "",
-			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=SiemensPLC_main.main.Server.NamespaceArray"}),
+			NodeIDs:                      ParseNodeIDs([]string{"ns=2;s=SiemensPLC_main.main.ServerInterfaces.Server _interface_1.counter"}),
 			BrowseHierarchicalReferences: true,
-			AutoReconnect:                true,
-			ReconnectIntervalInSeconds:   5,
-			ErrorOccured:                 false,
-			ExpectedValue:                "http://Server _interface_1",
-		}),
+			SubscribeEnabled:             true,
+		}, nil, true),
 	)
+
 })
