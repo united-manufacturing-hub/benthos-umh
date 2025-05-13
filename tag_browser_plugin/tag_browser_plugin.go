caa9c4cc34c3c4bd723eff7cce92fa6a2b5f6d0d
Ferdinand Linnenberg <fl@umh.app>
2025-05-13 16:16:39 +0200

    feat: metrics

--- tag_browser_plugin/tag_browser_plugin.go
+++ tag_browser_plugin/tag_browser_plugin.go
@@ -21,7 +21,10 @@ import (
 )
 
 type TagBrowserProcessor struct {
-	unsMapCache map[string]bool
+	logger            *service.Logger
+	messagesProcessed *service.MetricCounter
+	messagesErrored   *service.MetricCounter
+	unsMapCache       map[string]bool
 }
 
 func (t TagBrowserProcessor) Process(ctx context.Context, message *service.Message) (service.MessageBatch, error) {
@@ -35,7 +38,7 @@ func (t TagBrowserProcessor) Process(ctx context.Context, message *service.Messa
 	return messageBatch[0], nil
 }
 
-func (t TagBrowserProcessor) ProcessBatch(ctx context.Context, batch service.MessageBatch) ([]service.MessageBatch, error) {
+func (t TagBrowserProcessor) ProcessBatch(_ context.Context, batch service.MessageBatch) ([]service.MessageBatch, error) {
 	unsBundle := &tagbrowserpluginprotobuf.UnsBundle{
 		UnsMap: &tagbrowserpluginprotobuf.UnsMap{
 			Entries: make(map[string]*tagbrowserpluginprotobuf.UnsInfo),
@@ -50,11 +53,12 @@ func (t TagBrowserProcessor) ProcessBatch(ctx context.Context, batch service.Mes
 	for _, message := range batch {
 		unsInfo, eventTableEntry, unsTreeId, err := MessageToUNSInfoAndEvent(message)
 		if err != nil {
+			t.messagesErrored.Incr(1)
 			return nil, err
 		}
 
 		// This is safe, as unsTreeId will always be set if the error is nil
-		if t.unsMapCache[*unsTreeId] == false {
+		if _, ok := t.unsMapCache[*unsTreeId]; !ok {
 			unsBundle.UnsMap.Entries[*unsTreeId] = unsInfo
 			t.unsMapCache[*unsTreeId] = true
 		}
@@ -62,23 +66,43 @@ func (t TagBrowserProcessor) ProcessBatch(ctx context.Context, batch service.Mes
 
 		protoBytes, err := BundleToProtobufBytes(unsBundle)
 		if err != nil {
+			t.messagesErrored.Incr(1)
 			return nil, err
 		}
 
 		message.SetBytes(protoBytes)
 		resultBatch = append(resultBatch, message)
+		t.messagesProcessed.Incr(1)
 	}
 
 	return []service.MessageBatch{resultBatch}, nil
 }
 
-func (t TagBrowserProcessor) Close(ctx context.Context) error {
+func (t TagBrowserProcessor) Close(_ context.Context) error {
+	// Wipe cache
 	t.unsMapCache = make(map[string]bool)
 	return nil
 }
 
-func NewTagBrowserProcessor() *TagBrowserProcessor {
+func NewTagBrowserProcessor(logger *service.Logger, metrics *service.Metrics) *TagBrowserProcessor {
 	return &TagBrowserProcessor{
-		unsMapCache: make(map[string]bool),
+		logger:            logger,
+		messagesProcessed: metrics.NewCounter("messages_processed"),
+		messagesErrored:   metrics.NewCounter("messages_errored"),
+		unsMapCache:       make(map[string]bool),
+	}
+}
+
+func init() {
+	spec := service.NewConfigSpec().
+		Version("1.0.0").
+		Description("Processes messages into UNS bundles for the tag browser.").
+		Field(service.NewObjectField(""))
+
+	err := service.RegisterProcessor("tag_browser", spec, func(_ *service.ParsedConfig, mgr *service.Resources) (service.Processor, error) {
+		return NewTagBrowserProcessor(mgr.Logger(), mgr.Metrics()), nil
+	})
+	if err != nil {
+		panic(err)
 	}
 }
